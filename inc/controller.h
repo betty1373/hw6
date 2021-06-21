d0c51fa99d9a08664f7c6205e0c379a386de6ca8
betty1373 <61179097+betty1373@users.noreply.github.com>
2021-06-21 22:52:04 +0300

    Update controller.h

--- inc/controller.h
+++ inc/controller.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "value.h" 
-
-using namespace std;
 /// @file
 /// @brief  Class mediator between matrix object and their cells
 /// @author btv <example@example.com>      
@@ -18,7 +16,7 @@ using namespace std;
         {}
 /// @brief  Operator [] for access to matrix cell
         Value<T,defaultV> &operator[](size_t col) {
-            index_t ind = make_pair(m_r,col);
+            index_t ind {m_r,col};
             if (auto it = m_matrix.find(ind);
                      it!= m_matrix.end()) {
                 it->second.set_index(m_r,col);
